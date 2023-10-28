b86d3aec0c36b9a321a7068b9774719b86ea2f56
rummsi <rummsi@gmail.com>
2023-10-28 18:56:38 +0100

    - Correção <!DOCTYPE html> para o html5.

--- siteground-j16-37/index.php
+++ siteground-j16-37/index.php
@@ -29,7 +29,7 @@ HTMLHelper::_('script', 'js/jquery.js', ['version' => 'auto', 'relative' => true
 $app = Factory::getApplication();
 $tplparams	= $app->getTemplate(true)->params;
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 	<jdoc:include type="head" />
